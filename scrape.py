3070b788d79a9348c8272033b28b99db874e688f
Janine Wiesemann <119901184+JeanneDuPre@users.noreply.github.com>
2023-09-13 19:15:20 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -12,49 +12,46 @@ def fetch_articles(page):
     return soup.find_all('article', class_="paper_paper__1PY90 paper_outline__lwsUX card_card__lQWDv card_noPadding__D8PcU styles_reviewCard__hcAvl")
 
 def extract_article_data(articles):
-    article_list = []
-    for item in articles: 
-    # Extract data from each article item
-        author_element = item.find('span', class_= "typography_heading-xxs__QKBS8 typography_appearance-default__AAY17")
-        author = author_element.text if author_element else None
+    author_element = item.find('span', class_= "typography_heading-xxs__QKBS8 typography_appearance-default__AAY17")
+    author = author_element.text if author_element else None
         
-        land_element = item.find('div', class_= "typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_detailsIcon__Fo_ua")
-        land = land_element.text if land_element else None
+    land_element = item.find('div', class_= "typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_detailsIcon__Fo_ua")
+    land = land_element.text if land_element else None
         
-        date_element = item. find('p', class_= "typography_body-m__xgxZ_ typography_appearance-default__AAY17")
-        date = date_element.text if date_element else None
+    date_element = item. find('p', class_= "typography_body-m__xgxZ_ typography_appearance-default__AAY17")
+    date = date_element.text if date_element else None
         
-        text_element = item.find('p', class_= "typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn")
-        text = text_element.text if text_element else None
+    text_element = item.find('p', class_= "typography_body-l__KUYFJ typography_appearance-default__AAY17 typography_color-black__5LYEn")
+    text = text_element.text if text_element else None
         
-        nützlich_element = item.find('span', class_= "typography_body-m__xgxZ_ typography_appearance-inherit__D7XqR styles_usefulLabel__qz3JV")
-        nützlich = nützlich_element.text if nützlich_element else None
+    nützlich_element = item.find('span', class_= "typography_body-m__xgxZ_ typography_appearance-inherit__D7XqR styles_usefulLabel__qz3JV")
+    nützlich = nützlich_element.text if nützlich_element else None
         
-        title_element = item.find('h2', class_= "typography_heading-s__f7029 typography_appearance-default__AAY17")
-        title = title_element.text if title_element else None
+    title_element = item.find('h2', class_= "typography_heading-s__f7029 typography_appearance-default__AAY17")
+    title = title_element.text if title_element else None
         
-        bewertung_element = item.find('span', class_= "typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l")
-        bewertung = bewertung_element.text if bewertung_element else None
+    bewertung_element = item.find('span', class_= "typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l")
+    bewertung = bewertung_element.text if bewertung_element else None
 
-        sterne_element = item.find('div', class_="star-rating_starRating__4rrcf star-rating_medium__iN6Ty")
-        img_tag = sterne_element.find('img')  # Find the img tag within the 'sterne_element'
-        sterne_alt = img_tag.get('alt') if img_tag else None  # Extract the 'alt' attribute value
+    sterne_element = item.find('div', class_="star-rating_starRating__4rrcf star-rating_medium__iN6Ty")
+    img_tag = sterne_element.find('img')  # Find the img tag within the 'sterne_element'
+    sterne_alt = img_tag.get('alt') if img_tag else None  # Extract the 'alt' attribute value
 
         
-        date_online_element = item.find('time', class_= "data-service-review-date-time-ago")
-        date_online = date_online_element.text if date_online_element else None
+    date_online_element = item.find('time', class_= "data-service-review-date-time-ago")
+    date_online = date_online_element.text if date_online_element else None
 
-        article_data = {
-            'date': date,
-            'title': title,
-            'text': text,
-            'bewertung': bewertung,
-            'sterne': sterne_alt,
-            'author': author,
-            'land': land,
-            'nützlich': nützlich,
-            'date_online': date_online
-        }
+    article_data = {
+        'date': date,
+        'title': title,
+        'text': text,
+        'bewertung': bewertung,
+        'sterne': sterne_alt,
+        'author': author,
+        'land': land,
+        'nützlich': nützlich,
+        'date_online': date_online
+    }
 
     article_list.append(article_data)
     
@@ -73,9 +70,9 @@ def main():
 
     for page in range(start_page, end_page + 1): 
         articles = fetch_articles(page)
-    
-        article_data = extract_article_data(articles)
-        articlelist.extend(article_data)
+        for item in articles: 
+            article_data = extract_article_data(item)
+            articlelist.extend(article_data)
 
     df = pd.DataFrame(articlelist)
     # Define the CSV filename here
